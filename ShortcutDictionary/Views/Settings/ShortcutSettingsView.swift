fa74703731afe6141d156b425725e0b1ce3797d5
Afcoo <afcoodev@gmail.com>
2025-01-30 13:56:22 +0900

    손쉬운 사용 권한 확인 및 링크 추가

--- ShortcutDictionary/Views/Settings/ShortcutSettingsView.swift
+++ ShortcutDictionary/Views/Settings/ShortcutSettingsView.swift
@@ -6,6 +6,8 @@ struct ShortcutSettingsView: View {
     @AppStorage("enable_global_shortcut") var isGlobalShortcutEnabled: Bool = false
     @AppStorage("enable_copy_paste") var isCopyPasteEnabled: Bool = true
 
+    let accessEnabled = AXIsProcessTrusted() // 손쉬운 사용 권한 확인
+
     var body: some View {
         Form {
             // 전역 단축키
@@ -24,7 +26,18 @@ struct ShortcutSettingsView: View {
             // 단축키 입력시 복사 유무 결정
             Toggle(isOn: $isCopyPasteEnabled) {
                 Text("선택된 단어 바로 검색")
-                Text("단축키 입력시 자동으로 복사&붙여넣기를 합니다")
+                if accessEnabled {
+                    Text("단축키 입력시 자동으로 복사&붙여넣기를 합니다")
+                }
+                else {
+                    Text("이 기능을 사용하려면 '시스템 설정 > 개인정보 보호 및 보안 > 손쉬운 사용'에서 단축키 사전을 허용해주세요")
+                    Button(action: {
+                        NSWorkspace.shared.open(URL(string: "x-apple.systempreferences:com.apple.preference.security?Privacy_Accessibility")!)
+                    }) {
+                        Text("설정 열기").underline()
+                    }
+                    .buttonStyle(.borderless)
+                }
             }
             //                .padding(.leading, 20)
             .disabled(!isGlobalShortcutEnabled)
