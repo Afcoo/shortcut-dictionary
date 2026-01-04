fe94ef3a381016cdc21d7761c4227936df76b3c1
Afcoo <afcoodev@gmail.com>
2026-01-05 00:09:45 +0900

    새로운 사전 뷰 적용
    - 레이아웃 ZStack으로 변경
    - 새로운 Liquid Glass 툴바 대응
    - 유동적으로 padding을 조절 가능하게 변경

--- ShortcutDictionary/Views/DictionaryView.swift
+++ ShortcutDictionary/Views/DictionaryView.swift
@@ -7,26 +7,42 @@ struct DictionaryView: View {
     @AppStorage(SettingKeys.isToolbarEnabled.rawValue)
     private var isToolbarEnabled = SettingKeys.isToolbarEnabled.defaultValue as! Bool
 
-    var _padding = 8.0
+    @AppStorage(SettingKeys.dictViewPadding.rawValue)
+    private var dictViewPadding = SettingKeys.dictViewPadding.defaultValue as! Double
+
+    @AppStorage(SettingKeys.isLiquidGlassEnabled.rawValue)
+    private var isLiquidGlassEnabled = SettingKeys.isLiquidGlassEnabled.defaultValue as! Bool
 
     var body: some View {
-        VStack {
-            // 툴바
-            if isToolbarEnabled {
-                DictToolbar()
-                    .accessibilitySortPriority(1)
-                Spacer()
-                    .frame(height: _padding)
-            }
-
+        ZStack(alignment: .topLeading) {
             // 사전 웹 뷰
             if WebDictManager.shared.getDict(selectedDict) != nil {
                 WebDictView(webDict: WebDictManager.shared.getDict(selectedDict)!)
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .clipShape(RoundedRectangle(cornerRadius:
+                        isLiquidGlassEnabled
+                            ? max(26.0 - dictViewPadding, 14.0)
+                            : max(15.0 - dictViewPadding, 10.0)
+                    ))
                     .accessibilitySortPriority(2) // 사전에 우선적 포커스
+                    .padding([.horizontal, .bottom], dictViewPadding)
+                    .padding(.top, (!isLiquidGlassEnabled && isToolbarEnabled) ? 36.0 : dictViewPadding)
+            }
+
+            // 툴바
+            if isToolbarEnabled {
+                if #available(macOS 26.0, *), isLiquidGlassEnabled {
+                    // 신버전 툴바 (Liquid Glass)
+                    DictToolbarV2()
+                        .accessibilitySortPriority(1)
+                        .gesture(WindowDragGesture()) // 툴바로도 윈도우를 움직일 수 있게 설정
+                        .padding(.all, dictViewPadding)
+                } else {
+                    DictToolbar()
+                        .accessibilitySortPriority(1)
+                        .padding(.all, 8.0)
+                }
             }
         }
-        .padding(_padding)
         .background { ColoredBackground().ignoresSafeArea() }
         .setDictViewContextMenu() // Edge 우클릭 시 메뉴 표시
     }
